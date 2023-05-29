1b85272da917c5096a77e5b4cf1f979d958325a0
Tomas Licenblat <tlicenblat@frba.utn.edu.ar>
2023-05-28 21:44:07 -0300

    Update hello.c

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main() {
-   FILE *fp;
-   fp = fopen("output.txt", "w");
-    fprintf(fp, "Hello, World!\n");
+
     printf("Hello, World!\n");
-    fclose(fp);
+
    return 0;
 }
